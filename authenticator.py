55a056bb1ae6ff70280612629418d7af34238608
Ira <immortaltapok@lineardev.net>
2023-10-19 18:53:56 +0300

    add authenticator.py

---
+++ authenticator.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+import os
+
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=False, slow_mo=50)
+    page = browser.new_page()
+    page.goto('https://claude.ai/login')
+
+    input()
+
+    cookies = page.context.cookies()
+
+    cookies_str = "; ".join([f"{cookie['name']}={cookie['value']}" for cookie in cookies])
+    os.environ["COOKIE"] = cookies_str
+    with open('.env', 'w') as env_file:
+        env_file.write(f"COOKIE={os.environ['COOKIE']}")
