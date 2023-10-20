1007cfc074bab604db8f4686458f6aaa25a66a6e
Ira <immortaltapok@lineardev.net>
2023-10-20 16:34:35 +0300

    change email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='python_claude_web_authenticator',
     version='1.1.1',
     author='Immortal Tapok',
-    author_email='immortaltapok@lineardev.net',
+    author_email='irisa1704@gmail.com',
     py_modules=['authenticator', 'connect'],
     install_requires=['playwright', 'python-dotenv', 'claude-api']
 )
