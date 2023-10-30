0da95172a2bcee91d47b6d94f1f9e4da22cc0af1
Ira <immortaltapok@lineardev.net>
2023-10-30 21:07:12 +0200

    update dialog.py generate dialog title

--- dialog.py
+++ dialog.py
@@ -12,4 +12,8 @@ class ClaudeDialog:
         prompt = data[0]
         conversation_id = claude_api.create_new_chat()['uuid']
         response = claude_api.send_message(prompt, conversation_id)
+        title_chat_id = claude_api.create_new_chat()['uuid']
+        title = claude_api.send_message(f'Generate only a title for this text: {response}', title_chat_id)
+        claude_api.rename_chat(title, conversation_id)
+        claude_api.delete_conversation(title_chat_id)
         return [response]
