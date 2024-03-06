d94897bfd5519bebdeabdf2aaa612304837017b1
Ira <immortaltapok@lineardev.net>
2024-03-06 16:16:38 +0200

    update ClaudeClientOverride method get_organization_id, send_message, create_new_chat

--- claude_client_override.py
+++ claude_client_override.py
@@ -1,11 +1,32 @@
 import json
 import re
-
 from curl_cffi import requests
 from claude_api import Client
 
 
 class ClaudeClientOverride(Client):
+
+    def get_organization_id(self):
+        url = "https://claude.ai//api/auth/current_account"
+
+        headers = {
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://claude.ai/login',
+            'Content-Type': 'application/json',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'Connection': 'keep-alive',
+            'Cookie': f'{self.cookie}'
+        }
+
+        response = requests.get(url, headers=headers, impersonate="chrome110")
+        res = json.loads(response.text)
+        uuid = next(iter(res['messageLimits']))
+
+        return uuid
 
     # Send Message to Claude
     def send_message(self, prompt, conversation_id, attachment=None, timeout=500):
@@ -30,21 +51,20 @@ class ClaudeClientOverride(Client):
             "prompt": f"{prompt}",
             "attachments": attachments,
             "files": [],
-            "timezone": "Asia/Kolkata",
-            "model": "claude-2.1"
+            "timezone": "Asia/Kolkata"
 
         })
 
         headers = {
+            'Accept-Encoding': 'gzip, deflate, br',
            'User-Agent':
                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
             'Accept': 'text/event-stream, text/event-stream',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Referer': f'https://claude.ai/chats',
+            'Accept-Language': 'en-US;q=0.5,en;q=0.3',
+            'Alt-Used': 'claude.ai',
+            'Referer': f'https://claude.ai/chat/{self.organization_id}',
             'Content-Type': 'application/json',
             'Origin': 'https://claude.ai',
-            'DNT': '1',
-            'Connection': 'keep-alive',
             'Cookie': f'{self.cookie}',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
@@ -98,3 +118,33 @@ class ClaudeClientOverride(Client):
             return True
         else:
             return False
+
+    def create_new_chat(self):
+        url = f"https://claude.ai/api/organizations/{self.organization_id}/chat_conversations"
+        uuid = self.generate_uuid()
+
+        payload = json.dumps({"uuid": uuid, "name": ""})
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'anthropic-client-sha': 'unknown',
+            'anthropic-client-version': 'unknown',
+            'Alt-Used': 'claude.ai',
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0',
+            'Accept-Language': 'en-US;q=0.5,en;q=0.3',
+            'Referer': 'https://claude.ai/chats',
+            'Content-Type': 'application/json',
+            'Origin': 'https://claude.ai',
+            'Cookie': f'{self.cookie}',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'TE': 'trailers',
+            }
+
+        response = requests.post(url, headers=headers, data=payload, impersonate="chrome110")
+
+
+        # Returns JSON of the newly created conversation information
+        return response.json()
