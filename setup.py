7df55d64de8d8e8ab978a3a890606bc58f626b41
Ira <immortaltapok@lineardev.net>
2024-05-21 18:02:22 +0300

    fixed the 403 error and added saving organization_id to .env

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='python_claude_web_authenticator',
-    version='1.2.0',
+    version='1.2.1',
     author='Immortal Tapok',
     author_email='irisa1704@gmail.com',
     py_modules=['authenticator', 'dialog', 'claude_client_override'],
-    install_requires=['python-dotenv', 'requests'],
+    install_requires=['python-dotenv', 'requests', 'httpx'],
     extras_require={
         'authenticator': ['playwright']
     }
