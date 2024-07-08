482a1f475d29f08d8edb9e039dae6bb92118d17d
Ira <immortaltapok@lineardev.net>
2024-07-08 18:58:10 +0300

    Improved CLAUDE_COOKIE check in .env

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python_claude_web_authenticator',
-    version='1.2.3',
+    version='1.2.4',
     author='Immortal Tapok',
     author_email='irisa1704@gmail.com',
     py_modules=['authenticator', 'dialog', 'claude_client_override'],
